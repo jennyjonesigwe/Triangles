3ad1da2c6af2f033f476304dc35f16fd57b3d9d9
jennyjonesigwe <jennyjonesigwe@gmail.com>
2024-09-02 17:30:08 +0100

    Triangles

---
+++ Triangles.js
@@ -0,0 +1,27 @@
+
+const side1 = 4;
+const side2 = 3;
+const side3 = 2;
+
+if (side1 === side2 && side2 === side3)
+
+{
+  console.log("This is an Equitorial triangle" + ".");
+}
+
+else if (side1 === side2 || side1 === side3 || side2 === side3)
+
+{
+  console.log("This is an Isosceles triangle" + ".");
+
+}
+// }
+
+// else if (!(side1 == side2 || side1 == side3 || side2 == side3))
+
+// {
+
+  else {
+    
+  console.log("This is a Scalene triangle" + ".")
+}
